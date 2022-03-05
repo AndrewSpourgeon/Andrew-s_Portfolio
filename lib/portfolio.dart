07c5c230b1699d9eeb4bf50972af83ace9fd2fba
Andrew Spourgeon <andrewspourgeon@gmail.com>
2022-03-05 21:02:03 +0530

    Network Image

--- lib/portfolio.dart
+++ lib/portfolio.dart
@@ -959,7 +959,8 @@ class _PortfolioState extends State<Portfolio> {
                       width: 90,
                       decoration: BoxDecoration(
                           image: DecorationImage(
-                              image: AssetImage('assets/ui.png')),
+                              image: NetworkImage(
+                                  'https://firebasestorage.googleapis.com/v0/b/drew-portfolio.appspot.com/o/ui.png?alt=media&token=cab8bba4-9ba6-4009-b0db-9edd38d20c05')),
                           borderRadius: BorderRadius.circular(80)),
                     ),
                   ],
@@ -1179,7 +1180,7 @@ class _PortfolioState extends State<Portfolio> {
                                 decoration: BoxDecoration(
                                   borderRadius: BorderRadius.circular(8),
                                   image: DecorationImage(
-                                      image: AssetImage(data[index].image!),
+                                      image: NetworkImage(data[index].image!),
                                       fit: BoxFit.cover),
                                 ),
                               ),
@@ -2159,7 +2160,7 @@ class ContactItem extends StatelessWidget {
             BoxShadow(
                 color: Colors.grey, offset: Offset(-1, -1), blurRadius: 15)
           ]),
-      height: 220,
+      height: 235,
       width: 150,
       child: Center(
         child: Column(
